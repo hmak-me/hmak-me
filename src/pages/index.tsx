069d18050f05f6abab6678bcdc3cf53c3206c0b6
Hossein Maktoobian <8bitsight@gmail.com>
2020-12-07 14:34:00 +0330

    Create home page texts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,24 @@ import React from "react"
 import Layout from "../layout"
 import SEO from "../components/seo"
 import styles from "./index.module.styl"
+import Button from "../components/button"
+import { Link } from "gatsby"
 
 const HomePage = () => (
-  <Layout>
+  <Layout className={styles.homePage}>
     <SEO title="Home" />
+
+    <div className="container">
+      <div className={styles.info}>
+        <div className={styles.hi}>Hi, I'm</div>
+        <div className={styles.name}>Hossein</div>
+        <div className={styles.jobTitle}>Web Developer</div>
+      </div>
+
+      <Link to='/cv'>
+        <Button>Download CV</Button>
+      </Link>
+    </div>
   </Layout>
 )
 
